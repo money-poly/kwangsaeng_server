0ed90d2dc86d22be320cf8e6b3df097a3fa5f851
seunggeon <tmdrjs9555@naver.com>
2023-11-08 17:07:14 +0900

    fix: add secretkey
    git action test를 위한 push

---
+++ src/auth/config/secretkey.ts
@@ -0,0 +1,6 @@
+export const jwtConstants = {
+    access_secret: 'ACCESS_SECRET_KWANGSAENG',
+    access_expires: '30m',
+    refresh_secret: 'REFRESH_SECRET_KWANGSAENG',
+    refresh_expires: '7d',
+};

29b8155e607d699b969a32c31616847677d4b9d3
KKardd <wlstjd4013@gmail.com>
2023-11-23 15:32:11 +0900

    feat: integrate dynamo module for data storage

--- src/stores/stores.module.ts
+++ src/stores/stores.module.ts
@@ -6,9 +6,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Store } from './entity/store.entity';
 import { UsersRepository } from 'src/users/users.repository';
 import { User } from 'src/users/entity/user.entity';
+import { DynamooseModule } from 'nestjs-dynamoose';
+import { DynamoSchema } from './dynamo.schema';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Store, User])],
+    imports: [
+        TypeOrmModule.forFeature([Store, User]),
+        DynamooseModule.forFeature([{ name: 'Store-Menu', schema: DynamoSchema }]),
+    ],
     controllers: [StoresController],
     providers: [StoresService, StoresRepository, UsersRepository],
     exports: [StoresService],

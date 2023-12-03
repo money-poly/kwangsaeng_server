5d00cb22bd1e7425bc25a066831320e64bde5719
KKardd <wlstjd4013@gmail.com>
2023-12-04 00:24:12 +0900

    refactor: modify menu status options

--- src/menus/enum/menu-status.enum.ts
+++ src/menus/enum/menu-status.enum.ts
@@ -1,6 +1,5 @@
 export enum MenuStatus {
-    ON_DISCOUNT = '할인 중',
-    OFF_DISCOUNT = '할인 종료',
-    ON_SALE = '판매 중',
-    OFF_SALE = '판매 종료',
+    SALE = '판매 중',
+    HIDDEN = '숨김',
+    STAY = '유지',
 }

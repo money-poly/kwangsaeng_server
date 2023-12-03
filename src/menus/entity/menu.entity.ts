11230bf6dc45ca4c78644ef45e6baf274aecc190
KKardd <wlstjd4013@gmail.com>
2023-12-04 00:23:48 +0900

    refactor: modify columns(create count, tag / remove countryOfOrigin)

--- src/menus/entity/menu.entity.ts
+++ src/menus/entity/menu.entity.ts
@@ -1,6 +1,8 @@
 import { AbstractEntity } from 'src/global/common/abstract.entity';
 import { Store } from 'src/stores/entity/store.entity';
-import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { MenuTags } from '../enum/menu-tag.enum';
+import { MenuStatus } from '../enum/menu-status.enum';
 
 @Entity({ name: 'menus' })
 export class Menu extends AbstractEntity<Menu> {
@@ -13,23 +15,26 @@ export class Menu extends AbstractEntity<Menu> {
     @Column({ comment: '메뉴의 할인 전 가격' })
     price: number;
 
-    @Column({ comment: '메뉴 사진 URL' })
+    @Column({ name: 'count', comment: '판매 예정 개수', default: 0 })
+    count: number;
+
+    @Column({ type: 'enum', enum: MenuTags, comment: '메뉴 할인 태그', nullable: true })
+    tag: MenuTags;
+
+    @Column({ comment: '메뉴 사진 URL', nullable: true })
     menuPictureUrl: string;
 
-    @Column({ comment: '인기 메뉴 여부' })
+    @Column({ comment: '인기 메뉴 여부', nullable: true })
     popularity: boolean;
 
-    @Column()
-    status: string;
+    @Column({ type: 'enum', enum: MenuStatus, comment: '메뉴 상태' })
+    status: MenuStatus;
 
-    @Column({ name: 'expired_date', comment: '메뉴 유통기한' })
+    @Column({ name: 'expired_date', comment: '메뉴 유통기한', nullable: true })
     expiredDate: Date;
 
-    @Column({ name: 'view_count', comment: '조회수' })
+    @Column({ name: 'view_count', comment: '조회수', default: 0 })
     viewCount: number;
-
-    @Column({ name: 'country_of_origin', comment: '원산지 표기' })
-    countryOfOrigin: string;
 
     @Column({ comment: '메뉴에 관한 설명' })
     description: string;

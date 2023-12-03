b44f4ac8914fe20c0d24f6ecd185ebca09f9249c
KKardd <wlstjd4013@gmail.com>
2023-12-04 00:25:05 +0900

    feat: modify dto for calculating distance

--- src/menus/dto/find-one-menu.dto.ts
+++ src/menus/dto/find-one-menu.dto.ts
@@ -1,7 +1,15 @@
-import { IsNotEmpty, IsNumberString } from 'class-validator';
+import { IsLatitude, IsLongitude, IsNotEmpty, IsNumberString } from 'class-validator';
 
 export class FindOneMenuDto {
     @IsNumberString()
     @IsNotEmpty()
     id: number;
+
+    @IsLatitude()
+    @IsNotEmpty()
+    lat: number;
+
+    @IsLongitude()
+    @IsNotEmpty()
+    lon: number;
 }

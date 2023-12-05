c5dc5a65859765dbfafc7f1608ccac312ffd4349
KKardd <wlstjd4013@gmail.com>
2023-12-05 14:30:49 +0900

    feat: add routing for find-detail-menu service

--- src/menus/menus.controller.ts
+++ src/menus/menus.controller.ts
@@ -1,13 +1,20 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
-import { MenusService } from './menus.service';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { MenusService } from 'src/menus/menus.service';
 import { AuthGuard } from 'src/auth/guard/auth.guard';
 import { CurrentUser } from 'src/global/decorator/current-user.decorator';
 import { User } from 'src/users/entity/user.entity';
 import { CreateMenuDto } from './dto/create-menu.dto';
+import { FindOneMenuDto } from './dto/find-one-menu.dto';
 
 @Controller('menus')
 export class MenusController {
     constructor(private readonly menusService: MenusService) {}
+
+    @Get('/detail')
+    async findDetailOne(@Body() dto: FindOneMenuDto) {
+        const { id, lat, lon } = dto;
+        return this.menusService.findDetailOne(id, lat, lon);
+    }
 
     @Post()
     @UseGuards(AuthGuard)

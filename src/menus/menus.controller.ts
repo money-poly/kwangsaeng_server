c38d7e3b2ad4d32b7cc8e6f98b994b7de3cc89a4
KKardd <wlstjd4013@gmail.com>
2023-12-06 11:02:49 +0900

    feat: add controller about menu-update service

--- src/menus/menus.controller.ts
+++ src/menus/menus.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { MenusService } from 'src/menus/menus.service';
 import { AuthGuard } from 'src/auth/guard/auth.guard';
 import { CurrentUser } from 'src/global/decorator/current-user.decorator';
 import { User } from 'src/users/entity/user.entity';
-import { CreateMenuDto } from './dto/create-menu.dto';
 import { FindOneMenuDto } from './dto/find-one-menu.dto';
+import { CreateMenuDto } from './dto/create-menu.dto';
+import { UpdateMenuDto } from './dto/update-menu.dto';
 
 @Controller('menus')
 export class MenusController {
@@ -20,5 +21,11 @@ export class MenusController {
     @UseGuards(AuthGuard)
     async create(@CurrentUser() user: User, @Body() dto: CreateMenuDto) {
         return await this.menusService.create(user, dto);
+    }
+
+    @Put('/:id')
+    @UseGuards(AuthGuard)
+    async update(@Param() menuId: number, @Body() dto: UpdateMenuDto) {
+        return await this.menusService.update(menuId, dto);
     }
 }

b107951daa96b2fd8748425133b18d736a6aa11c
KKardd <wlstjd4013@gmail.com>
2023-12-23 17:34:37 +0900

    feat: add repository options in menu

--- src/menus/menus.repository.ts
+++ src/menus/menus.repository.ts
@@ -1,18 +1,50 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { EntityManager, Repository } from 'typeorm';
-import { AbstractRepository } from 'src/global/common/abstract.repository';
+import {
+    EntityManager,
+    FindManyOptions,
+    FindOptionsWhere,
+    Repository,
+    FindOptionsSelect,
+    FindOptionsRelations,
+} from 'typeorm';
 import { Menu } from './entity/menu.entity';
+import { MenuView } from './entity/menu-view.entity';
+import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
 
 @Injectable()
-export class MenusRepository extends AbstractRepository<Menu> {
+export class MenusRepository {
     protected readonly logger = new Logger(MenusRepository.name);
 
     constructor(
         @InjectRepository(Menu)
-        menusRepository: Repository<Menu>,
-        entityManager: EntityManager,
+        private readonly menus: Repository<Menu>,
+        @InjectRepository(MenuView)
+        private readonly menuView: Repository<MenuView>,
+        public entityManager: EntityManager,
+    ) {}
+
+    async findOneMenu(
+        where: FindOptionsWhere<Menu>,
+        select?: FindOptionsSelect<Menu>,
+        relations?: FindOptionsRelations<Menu>,
     ) {
-        super(menusRepository, entityManager);
+        return await this.menus.findOne({
+            where,
+            select,
+            relations,
+        });
+    }
+
+    async existMenu(where: FindManyOptions<Menu>) {
+        return await this.menus.exist(where);
+    }
+
+    async updateStore(menu: Menu, partialEntity: QueryDeepPartialEntity<Menu>) {
+        await this.menus.update(menu.id, partialEntity);
+
+        return await this.menus.findOneBy({
+            id: menu.id,
+        });
     }
 }

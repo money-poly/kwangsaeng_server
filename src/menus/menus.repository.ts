13f54f897e510c779786d8d1a7fc293c6bb65146
KKardd <wlstjd4013@gmail.com>
2023-12-23 19:11:34 +0900

    fix: add create-menu service

--- src/menus/menus.repository.ts
+++ src/menus/menus.repository.ts
@@ -11,6 +11,9 @@ import {
 import { Menu } from './entity/menu.entity';
 import { MenuView } from './entity/menu-view.entity';
 import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
+import { User } from 'src/users/entity/user.entity';
+import { CreateMenuDto } from './dto/create-menu.dto';
+import { CreateMenuArgs } from './interface/create-menu.interface';
 
 @Injectable()
 export class MenusRepository {
@@ -24,7 +27,7 @@ export class MenusRepository {
         public entityManager: EntityManager,
     ) {}
 
-    async findOneMenu(
+    async findOne(
         where: FindOptionsWhere<Menu>,
         select?: FindOptionsSelect<Menu>,
         relations?: FindOptionsRelations<Menu>,
@@ -36,15 +39,27 @@ export class MenusRepository {
         });
     }
 
-    async existMenu(where: FindManyOptions<Menu>) {
+    async exist(where: FindManyOptions<Menu>) {
         return await this.menus.exist(where);
     }
 
-    async updateStore(menu: Menu, partialEntity: QueryDeepPartialEntity<Menu>) {
+    async update(menu: Menu, partialEntity: QueryDeepPartialEntity<Menu>) {
         await this.menus.update(menu.id, partialEntity);
 
         return await this.menus.findOneBy({
             id: menu.id,
         });
+    }
+
+    async create(user: User, args: CreateMenuArgs) {
+        const newMenu = this.menus.create({
+            ...args,
+            view: this.menuView.create({
+                viewCount: 0,
+            }),
+        });
+        await this.menus.save(newMenu);
+
+        return newMenu.id;
     }
 }

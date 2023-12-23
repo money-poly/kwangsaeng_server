8e9e0deedd03b019657b6a3f9197b4d2a6b6156b
KKardd <wlstjd4013@gmail.com>
2023-12-23 17:50:04 +0900

    feat: add create-menu validation pipe

---
+++ src/menus/pipe/create-menu-validation.pipe.ts
@@ -0,0 +1,30 @@
+import { MenusException } from 'src/global/exception/menus-exception';
+import { CreateMenuDto } from '../dto/create-menu.dto';
+import { MenusService } from '../menus.service';
+import { Injectable, PipeTransform } from '@nestjs/common';
+
+@Injectable()
+export class CreateMenuValidationPipe implements PipeTransform {
+    constructor(private readonly menusService: MenusService) {}
+
+    async transform(dto: CreateMenuDto) {
+        await this.validationName(dto.storeId, dto.name);
+
+        return dto;
+    }
+
+    async validationName(id: number, name: string) {
+        const exist = await this.menusService.existMenu({
+            where: {
+                name,
+                store: {
+                    id,
+                },
+            },
+        });
+
+        if (exist) {
+            throw MenusException.ALREADY_EXIST_MENU_NAME;
+        }
+    }
+}

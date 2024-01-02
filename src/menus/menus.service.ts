e4907796e3f454f3ff36359abc36309bdb21dd81
KKardd <wlstjd4013@gmail.com>
2024-01-02 20:48:06 +0900

    refactor: add columns in getMenusInStore method

--- src/menus/menus.service.ts
+++ src/menus/menus.service.ts
@@ -305,7 +305,7 @@ export class MenusService {
         return await this.entityManager
             .createQueryBuilder(Menu, 'menus')
             .select(
-                'menus.menu_picture_url AS menuPictureUrl, menus.id AS menuId, menus.name, menus.discount_rate AS discountRate, menus.price',
+                'menus.menu_picture_url AS menuPictureUrl, menus.id AS menuId, menus.name, menus.discount_rate AS discountRate, menus.price, menus.description',
             )
             .where('menus.id != :excludeMenuId AND store_id = :storeId', { excludeMenuId, storeId })
             .orderBy('discountRate', 'DESC')

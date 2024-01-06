7900c6962528a07a02a81ae9f6da769b4469d1df
KKardd <wlstjd4013@gmail.com>
2024-01-07 01:52:29 +0900

    feat: add relation option in transform-menu pipe

--- src/menus/pipe/transform-menu.pipe.ts
+++ src/menus/pipe/transform-menu.pipe.ts
@@ -8,14 +8,17 @@ export class TransformMenuPipe implements PipeTransform {
     constructor(private readonly menusService: MenusService) {}
 
     async transform(value: number) {
-        const menu = await this.menusService.findOne({
-            id: value,
-        });
+        const menu = await this.menusService.findOne(
+            {
+                id: value,
+            },
+            {},
+            { store: true },
+        );
 
         if (!menu) {
             throw MenusException.ENTITY_NOT_FOUND;
         }
-
         return menu;
     }
 }

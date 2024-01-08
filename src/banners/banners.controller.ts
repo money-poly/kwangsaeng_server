1a5d04d3b6c6188c690b11222ec653b04fb198a9
KKardd <wlstjd4013@gmail.com>
2024-01-08 14:55:56 +0900

    feat: add upload-image controller in banner

--- src/banners/banners.controller.ts
+++ src/banners/banners.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { BannersService } from './banners.service';
+import { AuthGuard } from 'src/auth/guard/auth.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CreateBannerDto } from './dto/create-banner.dto';
 
 @Controller('banners')
-export class BannersController {}
+export class BannersController {
+    constructor(private readonly bannerService: BannersService) {}
+
+    @Post('/upload')
+    @UseGuards(AuthGuard) // TODO: 어드민 권한
+    @UseInterceptors(FileInterceptor('file'))
+    async uploadImage(@UploadedFile() file: Express.MulterS3.File, @Body() formData: object) {
+        const dto: CreateBannerDto = JSON.parse(JSON.stringify(formData));
+        await this.bannerService.create(dto, file);
+    }
+}

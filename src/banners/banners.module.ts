fef0d061e75eefdf8e818e9eb9682c4cb019aec0
KKardd <wlstjd4013@gmail.com>
2024-01-08 14:32:53 +0900

    feat: DI database modules

--- src/banners/banners.module.ts
+++ src/banners/banners.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { BannersService } from './banners.service';
 import { BannersController } from './banners.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Banner } from './entity/banner.entity';
+import { MulterModule } from '@nestjs/platform-express';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { multerS3Config } from 'src/global/config/multer-s3.config';
 
 @Module({
-  providers: [BannersService],
-  controllers: [BannersController]
+    imports: [
+        TypeOrmModule.forFeature([Banner]),
+        MulterModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => multerS3Config(configService),
+        }),
+    ],
+    providers: [BannersService],
+    controllers: [BannersController],
 })
 export class BannersModule {}

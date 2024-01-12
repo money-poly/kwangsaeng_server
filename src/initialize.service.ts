5b402fd2a6d556b7d926faf6e3d729c7ffdb07fc
Junsung Choi <choiexe1@gmail.com>
2024-01-12 21:28:20 +0900

    KS-130: app module, initialize.service 수정

--- src/initialize.service.ts
+++ src/initialize.service.ts
@@ -2,6 +2,7 @@ import { CategoriesService } from './categories/categories.service';
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { StoresService } from './stores/stores.service';
 import { MenusService } from './menus/menus.service';
+import { TagsService } from './tags/tags.service';
 
 @Injectable()
 export class InitializeService implements OnModuleInit {
@@ -9,10 +10,12 @@ export class InitializeService implements OnModuleInit {
         private readonly categoriesService: CategoriesService,
         private readonly storesService: StoresService,
         private readonly menusService: MenusService,
+        private readonly tagsService: TagsService,
     ) {}
 
     async onModuleInit() {
         await this.categoriesService.initCategories();
+        await this.tagsService.initTags();
         await this.storesService.initMockStores();
         await this.menusService.initMockMenus();
         await this.storesService.migrateDynamo();

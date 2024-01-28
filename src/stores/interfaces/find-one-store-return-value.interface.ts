1178d2cd2a9a7d62be34255df77601cf686079f1
KKardd <wlstjd4013@gmail.com>
2024-01-28 22:13:56 +0900

    feat: declare return value in find-one-store service

---
+++ src/stores/interfaces/find-one-store-return-value.interface.ts
@@ -0,0 +1,23 @@
+import { Category } from 'src/categories/entity/category.entity';
+import { Menu } from 'src/menus/entity/menu.entity';
+import { Store } from '../entity/store.entity';
+import { StoreDetail } from '../entity/store-detail.entity';
+import { User } from 'src/users/entity/user.entity';
+
+export interface FindOneStoreReturnValue {
+    id: Pick<Store, 'id'>;
+    name: Pick<Store, 'name'>;
+    categories: Pick<Category, 'name'>[];
+    detail: {
+        address: Pick<StoreDetail, 'address'>;
+        addressDetail: Pick<StoreDetail, 'addressDetail'>;
+        lat: Pick<StoreDetail, 'lat'>;
+        lon: Pick<StoreDetail, 'lon'>;
+        phone: Pick<User, 'phone'>;
+        cookingTime: Pick<StoreDetail, 'cookingTime'>;
+        operationTimes: Pick<StoreDetail, 'operationTimes'>;
+        pickupTime: string;
+        menuOrders: Pick<StoreDetail, 'menuOrders'>;
+    };
+    menus?: Menu[];
+}

08eb8d7916708c1f4486ff613b719d943839f19c
KKardd <wlstjd4013@gmail.com>
2024-02-21 20:13:11 +0900

    refactor: remove image column in create-menu interface

--- src/menus/interface/create-menu.interface.ts
+++ src/menus/interface/create-menu.interface.ts
@@ -2,13 +2,12 @@ import { MenuStatus } from '../enum/menu-status.enum';
 
 export interface CreateMenuArgs {
     storeId: number;
-    image?: string;
+    menuPictureUrl?: string;
     name: string;
     description: string;
     status: MenuStatus;
     price: number;
-    countryOfOrigin?: { ingredient: string; origin: string }[];
     discountRate: number;
     salePrice: number;
-    menuPictureUrl?: string;
+    countryOfOrigin?: { ingredient: string; origin: string }[];
 }

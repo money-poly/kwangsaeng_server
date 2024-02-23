fd6ee295018b2c65a2c813462722609ad021ad38
KKardd <wlstjd4013@gmail.com>
2024-02-23 09:02:24 +0900

    refactor: modify error message about store-not-found

--- src/global/exception/stores-exception.ts
+++ src/global/exception/stores-exception.ts
@@ -10,7 +10,7 @@ export abstract class StoresException {
 
     static HAS_NO_PERMISSION_CREATE = new CommonException('가게 생성 권한이 없습니다.', 2001, HttpStatus.FORBIDDEN);
 
-    static ENTITY_NOT_FOUND = new CommonException('존재하지 않는 가게입니다.', 2002, HttpStatus.NOT_FOUND);
+    static ENTITY_NOT_FOUND = new CommonException('찾을 수 없는 가게입니다.', 2002, HttpStatus.NOT_FOUND);
 
     static ALREADY_HAS_STORE = new CommonException(
         '이미 가게를 소유하고 있습니다.',

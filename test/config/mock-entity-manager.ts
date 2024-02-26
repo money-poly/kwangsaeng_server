616cecb6c7d39ebaffa85c695af27979a330df6d
KKardd <wlstjd4013@gmail.com>
2024-02-26 14:08:30 +0900

    feat: declare mock-entity-manager

---
+++ test/config/mock-entity-manager.ts
@@ -0,0 +1,11 @@
+export const mockEntityManager = {
+    createQueryBuilder: jest.fn().mockReturnThis(),
+    select: jest.fn().mockReturnThis(),
+    from: jest.fn().mockReturnThis(),
+    where: jest.fn().mockReturnThis(),
+    andWhere: jest.fn().mockReturnThis(),
+    innerJoinAndSelect: jest.fn().mockReturnThis(),
+    leftJoinAndSelect: jest.fn().mockReturnThis(),
+    getMany: jest.fn().mockResolvedValue([]), // 예시: 빈 배열 반환
+    // 다른 필요한 메서드를 추가할 수 있습니다.
+};

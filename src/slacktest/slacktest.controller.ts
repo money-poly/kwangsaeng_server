69954d3661328154f8e825bb49d99b550621402d
softmoca <acyc0909@gmail.com>
2024-03-18 20:47:07 +0900

    test : module error apprunner slack alarm

--- src/slacktest/slacktest.controller.ts
+++ src/slacktest/slacktest.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class SlacktestController {
     @Get('test')
     pulltestrequest() {
-        return 'testtesttesttestt';
+        return 'testtesttesttestttttt';
     }
 }

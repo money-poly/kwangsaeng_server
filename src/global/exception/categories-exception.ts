c896132f5879a34d0b88e96860bec3d561775dab
KKardd <wlstjd4013@gmail.com>
2024-03-19 10:03:36 +0900

    feat: declare new exceptions in category

--- src/global/exception/categories-exception.ts
+++ src/global/exception/categories-exception.ts
@@ -9,4 +9,12 @@ export abstract class CategoriesException {
     );
 
     static PARENT_NOT_FOUND = new CommonException('존재하지 않는 대분류 카테고리입니다.', 5001, HttpStatus.NOT_FOUND);
+
+    static SUB_NOT_FOUND = new CommonException('존재하지 않는 소분류 카테고리입니다.', 5002, HttpStatus.NOT_FOUND);
+
+    static FIND_PARENT_CATEGORY = new CommonException(
+        '소분류 대신 대분류 카테고리를 검색했습니다.',
+        5003,
+        HttpStatus.BAD_REQUEST,
+    );
 }

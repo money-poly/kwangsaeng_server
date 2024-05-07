f17cedd516012c11a5ac5076041fe7400abc1ed5
KKardd <wlstjd4013@gmail.com>
2024-05-07 22:19:55 +0900

    feat: declare customer entity

---
+++ src/users/entity/customer.entity.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, OneToMany } from 'typeorm';
+import { SoftDeleteEntity } from 'src/global/common/abstract.entity';
+import { Order } from 'src/orders/entity/order.entity';
+
+@Entity({ name: 'customers' })
+export class Customer extends SoftDeleteEntity<Customer> {
+    @Column({ comment: 'firebase uid', unique: true })
+    fId: string;
+
+    @Column({ nullable: true, comment: '유저 이름' })
+    name: string;
+
+    @Column({ length: 15, comment: '전화번호 000-0000-0000' })
+    phone: string;
+
+    @Column({ comment: '문자 인증 유무', default: false })
+    isAuth: boolean;
+
+    @Column({ comment: '비회원 주문시 저장 비밀번호/회원시 null', nullable: true })
+    orderPassword: number;
+
+    @OneToMany(() => Order, (order) => order.customer)
+    order: Order;
+}

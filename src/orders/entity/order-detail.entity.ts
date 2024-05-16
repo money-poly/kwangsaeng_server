2bec1563a9d3069aea1f13c3b080aec366e23e47
KKardd <wlstjd4013@gmail.com>
2024-05-16 17:24:54 +0900

    refactor: add selling-price column in order-detail model

--- src/orders/entity/order-detail.entity.ts
+++ src/orders/entity/order-detail.entity.ts
@@ -1,11 +1,14 @@
-import { AbstractEntity } from 'src/global/common/abstract.entity';
-import { Column, Entity, ManyToOne, OneToOne } from 'typeorm';
+import { SoftDeleteEntity } from 'src/global/common/abstract.entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
 import { Order } from './order.entity';
 import { Menu } from 'src/menus/entity/menu.entity';
 
 @Entity({ name: 'order_detail' })
-export class OrderDetail extends AbstractEntity<OrderDetail> {
-    @Column({ comment: '단일 메뉴 가격' })
+export class OrderDetail extends SoftDeleteEntity<OrderDetail> {
+    @Column({ comment: '단일 메뉴 판매가' })
+    sellingPrice: number;
+
+    @Column({ comment: '단일 메뉴 원가' })
     price: number;
 
     @Column({ comment: '구매 수량', default: 1 })

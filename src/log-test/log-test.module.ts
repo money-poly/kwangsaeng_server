5bac782c07541c722567e4061e9735bc4842fc38
softmoca <acyc0909@gmail.com>
2024-05-21 15:58:31 +0900

    delete : log-test, slacktest directory

--- src/log-test/log-test.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LogTestService } from './log-test.service';
-import { LogTestController } from './log-test.controller';
-
-@Module({
-    controllers: [LogTestController],
-    providers: [LogTestService],
-})
-export class LogTestModule {}
--- src/slacktest/slacktest.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SlacktestController } from './slacktest.controller';
-import { SlacktestService } from './slacktest.service';
-
-@Module({
-  controllers: [SlacktestController],
-  providers: [SlacktestService]
-})
-export class SlacktestModule {}

e2b5d1432b79e6787e9b5cde43de5dc28c903796
softmoca <acyc0909@gmail.com>
2024-05-27 11:21:00 +0900

    fix : orderException CODE

--- src/global/exception/order-exceptoin.ts
+++ src/global/exception/order-exceptoin.ts
@@ -4,12 +4,12 @@ import { CommonException } from './common-exception';
 export abstract class OrderExceotion {
     static FAIL_ORDER_MENU = new CommonException(
         '주문 요청 오류 : 주문 요청 중에 오류가 발생했습니다.',
-        3008,
+        12001,
         HttpStatus.INTERNAL_SERVER_ERROR,
     );
     static FAIL_UNLOCK_REDIS = new CommonException(
         '레디스 LOCK 오류 : LOCK 해제에 실패했습니다.',
-        3009,
+        12002,
         HttpStatus.INTERNAL_SERVER_ERROR,
     );
 }

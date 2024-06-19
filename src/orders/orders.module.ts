cf4993c95a3d016b135a329ea4dd8f645693e40b
KKardd <wlstjd4013@gmail.com>
2024-06-19 22:30:18 +0900

    fix: combine order & orders module

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { Menu } from 'src/menus/entity/menu.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 
 @Module({
-  controllers: [OrdersController],
-  providers: [OrdersService]
+    imports: [TypeOrmModule.forFeature([Menu])],
+    controllers: [OrdersController],
+    providers: [OrdersService],
 })
 export class OrdersModule {}

d4cb77f152f5b8826aecdf7b0caa820818c5e9c9
KKardd <wlstjd4013@gmail.com>
2024-06-23 15:28:18 +0900

    feat: apply redis-module in orders module

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -3,9 +3,10 @@ import { Menu } from 'src/menus/entity/menu.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { RedisModule } from 'src/redis/redis.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Menu])],
+    imports: [TypeOrmModule.forFeature([Menu]), RedisModule],
     controllers: [OrdersController],
     providers: [OrdersService],
 })

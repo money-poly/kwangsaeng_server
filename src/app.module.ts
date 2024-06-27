7f1c6d0db67be6abe1419b3f910213dc53fa3163
KKardd <wlstjd4013@gmail.com>
2024-06-27 23:55:04 +0900

    refactor: DI redis-module in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,7 +18,7 @@ import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { TagsModule } from './tags/tags.module';
 import { SearchModule } from './search/search.module';
-import { RedisModule } from '@nestjs-modules/ioredis';
+import { RedisModule } from 'src/redis/redis.module';
 import { OrdersModule } from './orders/orders.module';
 
 @Module({

331d291172bc5f68cb7e4dc8d9776b03065f7cc0
KKardd <wlstjd4013@gmail.com>
2024-07-10 00:13:27 +0900

    feat: declare test code about menus 2.0

---
+++ test/menus/menus2.manager.spec.ts
@@ -0,0 +1,7 @@
+import { Menus2Manager } from '../../src/menus2.0/implement/menus2.manager';
+
+describe('Menus20', () => {
+    it('should be defined', () => {
+        expect(new Menus2Manager()).toBeDefined();
+    });
+});
---
+++ test/menus/menus2.reader.spec.ts
@@ -0,0 +1,7 @@
+import { Menus2Reader } from '../../src/menus2.0/implement/menus2.reader';
+
+describe('Menus20', () => {
+    it('should be defined', () => {
+        expect(new Menus2Reader()).toBeDefined();
+    });
+});

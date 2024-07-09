a611b17cffe2ea35502d9f896d9548184ffc9862
KKardd <wlstjd4013@gmail.com>
2024-07-10 00:24:59 +0900

    feat: configurate stores&menus module

--- src/menus2.0/menus2.module.ts
+++ src/menus2.0/menus2.module.ts
@@ -5,9 +5,33 @@ import { Menus2Appender } from './implement/menus2.appender';
 import { Menus2Reader } from './implement/menus2.reader';
 import { Menus2Manager } from './implement/menus2.manager';
 import { Menus2Repository } from './menus2.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MulterModule } from '@nestjs/platform-express';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { multerS3Config } from 'src/global/config/multer-s3.config';
+import { CategoriesModule } from 'src/categories/categories.module';
+import { Menu } from 'src/menus/entity/menu.entity';
+import { Store } from 'src/stores/entity/store.entity';
+import { Seller } from 'src/users/entity/seller.entity';
+import { MenuView } from 'src/menus/entity/menu-view.entity';
+import { StoreDetail } from 'src/stores/entity/store-detail.entity';
+import { BusinessDetail } from 'src/stores/entity/business-detail.entity';
+import { StoreApprove } from 'src/stores/entity/store-approve.entity';
+import { Stores2Reader } from 'src/stores2.0/implement/stores2.reader';
 
 @Module({
-    providers: [Menus2Service, Menus2Appender, Menus2Reader, Menus2Manager, Menus2Repository],
+    imports: [
+        TypeOrmModule.forFeature([Menu, Store, Seller, MenuView, StoreDetail, BusinessDetail, StoreApprove]),
+        MulterModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => multerS3Config(configService),
+        }),
+        // 초기단계에서는 스토어와 메뉴를 제외한 다른 모듈은 모듈 자체를 imports
+        // TODO) 아래의 모듈 마저도 implement layer를 통해 의존성 주입
+        CategoriesModule,
+    ],
+    providers: [Menus2Service, Menus2Appender, Menus2Reader, Menus2Manager, Menus2Repository, Stores2Reader],
     controllers: [Menus2Controller],
 })
 export class Menus2Module {}
--- src/stores2.0/stores2.module.ts
+++ src/stores2.0/stores2.module.ts
@@ -5,9 +5,35 @@ import { Stores2Appender } from './implement/stores2.appender';
 import { Stores2Manager } from './implement/stores2.manager';
 import { Stores2Reader } from './implement/stores2.reader';
 import { Stores2Repository } from './stores2.repository';
+import { Menus2Reader } from 'src/menus2.0/implement/menus2.reader';
+import { TagsModule } from 'src/tags/tags.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { multerS3Config } from 'src/global/config/multer-s3.config';
+import { HttpModule } from '@nestjs/axios';
+import { UsersModule } from 'src/users/users.module';
+import { CategoriesModule } from 'src/categories/categories.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Store } from 'src/stores/entity/store.entity';
+import { Seller } from 'src/users/entity/seller.entity';
+import { StoreDetail } from 'src/stores/entity/store-detail.entity';
+import { StoreApprove } from 'src/stores/entity/store-approve.entity';
+import { BusinessDetail } from 'src/stores/entity/business-detail.entity';
 
 @Module({
-    providers: [Stores2Service, Stores2Appender, Stores2Manager, Stores2Reader, Stores2Repository],
+    imports: [
+        TypeOrmModule.forFeature([Store, Seller, StoreDetail, BusinessDetail, StoreApprove]),
+        CategoriesModule,
+        UsersModule,
+        HttpModule,
+        TagsModule,
+        MulterModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => multerS3Config(configService),
+        }),
+    ],
+    providers: [Stores2Service, Stores2Appender, Stores2Manager, Stores2Reader, Stores2Repository, Menus2Reader],
     controllers: [Stores2Controller],
 })
 export class Stores2Module {}

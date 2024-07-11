0d372676cebe31a0b14ebd6e5d64061360d3ab1c
KKardd <wlstjd4013@gmail.com>
2024-07-11 17:56:27 +0900

    feat: declare todey-used-food-expenses dto

---
+++ src/menus2.0/dto/today-used-food-expenses.dto.ts
@@ -0,0 +1,23 @@
+import { Type } from 'class-transformer';
+import { IsBoolean, IsEnum, IsInt, IsLatitude, IsLongitude, IsOptional, IsString } from 'class-validator';
+import { SortFilterType } from '../enum/sort-filter-type.enum';
+
+export class TodayUsedFoodExpensesDto {
+    @IsInt()
+    @Type(() => Number)
+    amount: number;
+
+    @IsEnum(SortFilterType)
+    filter: SortFilterType;
+
+    @IsOptional()
+    @IsBoolean()
+    @Type(() => Boolean)
+    final: Boolean = false;
+
+    @IsLatitude()
+    lat: number;
+
+    @IsLongitude()
+    lon: number;
+}

2020fd3d7aa50d96c1bdf7e50e5ab3f2f62cb254
KKardd <wlstjd4013@gmail.com>
2024-07-11 17:52:46 +0900

    feat: add sorting-menu method in query-builder

--- src/menus2.0/menus2.repository.ts
+++ src/menus2.0/menus2.repository.ts
@@ -4,7 +4,7 @@ import { EntityManager, Repository, SelectQueryBuilder } from 'typeorm';
 import { MenuView } from 'src/menus/entity/menu-view.entity';
 import { Menu } from 'src/menus/entity/menu.entity';
 import { ExecuteQueryBuilderType } from 'src/global/common/execute-qb-type.enum';
-import { SortFilterType } from './enum/discounte-filter-type.enum';
+import { SortFilterType } from './enum/sort-filter-type.enum';
 
 @Injectable()
 export class Menus2Repository {
@@ -15,6 +15,25 @@ export class Menus2Repository {
         private readonly menuView: Repository<MenuView>,
         private readonly entityManager: EntityManager,
     ) {}
+
+    async sortInQb<T>(qb: SelectQueryBuilder<T>, type: SortFilterType) {
+        switch (type) {
+            case SortFilterType.DISCOUNT:
+                return qb.orderBy('discount_rate', 'DESC');
+            case SortFilterType.PRICE:
+                return qb.orderBy('price', 'ASC');
+            case SortFilterType.expire:
+                return qb.orderBy('expired_date', 'DESC');
+            case SortFilterType.popular:
+                if (
+                    // qb에 menuView가 이미 조인되어있는지 확인
+                    qb.expressionMap.joinAttributes.map((entity) => entity.entityOrProperty).includes('menu.menuView')
+                ) {
+                    return qb.orderBy('mv.view_count', 'DESC');
+                }
+                return qb.leftJoin(MenuView, 'mv').orderBy('mv.view_count', 'DESC');
+        }
+    }
 
     async executeQueryBuilder<T>(qb: SelectQueryBuilder<T>, type: ExecuteQueryBuilderType) {
         switch (type) {

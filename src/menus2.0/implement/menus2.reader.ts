22670b9af1bb9bc52dd2c434ffa1c274d6484ff2
KKardd <wlstjd4013@gmail.com>
2024-07-12 00:48:06 +0900

    feat: use today-used-food-expenses logic in menu reader

--- src/menus2.0/implement/menus2.reader.ts
+++ src/menus2.0/implement/menus2.reader.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Menus2Repository } from '../menus2.repository';
+import { SortFilterType } from '../enum/sort-filter-type.enum';
+import { ExecuteQueryBuilderType } from 'src/global/common/execute-qb-type.enum';
 
 @Injectable()
-export class Menus2Reader {}
+export class Menus2Reader {
+    constructor(private readonly menusRepository: Menus2Repository) {}
+
+    async readTodayUsedFoodExpenses(amount: number, filter: SortFilterType, final: boolean, lat: number, lon: number) {
+        const qb = await this.menusRepository.createQueryBuilder();
+
+        await this.menusRepository
+            .leftJoinMenuView(qb)
+            .then((qb) => this.menusRepository.leftJoinStore(qb))
+            .then((qb) => this.menusRepository.leftJoinStoreDetail(qb))
+            .then((qb) => this.menusRepository.filterDistance(qb, lat, lon))
+            .then((qb) => this.menusRepository.sortInQb(qb, filter))
+            .then((qb) => this.menusRepository.todayUsedFoodExpensesLogic(qb, amount, final));
+
+        return await this.menusRepository.executeQueryBuilder(qb, ExecuteQueryBuilderType.MANY);
+    }
+}

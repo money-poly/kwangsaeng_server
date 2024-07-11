fbe7ae1c1ebce964eea716287cf386a87f1295f4
KKardd <wlstjd4013@gmail.com>
2024-07-12 00:50:20 +0900

    feat: init DI store for repository layer in implement layer

--- src/stores2.0/implement/stores2.manager.ts
+++ src/stores2.0/implement/stores2.manager.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { Stores2Repository } from '../stores2.repository';
 
 @Injectable()
-export class Stores2Manager {}
+export class Stores2Manager {
+    constructor(private readonly storesRepository: Stores2Repository) {}
+}
--- src/stores2.0/implement/stores2.reader.ts
+++ src/stores2.0/implement/stores2.reader.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { Stores2Repository } from '../stores2.repository';
 
 @Injectable()
-export class Stores2Reader {}
+export class Stores2Reader {
+    constructor(private readonly storesRepository: Stores2Repository) {}
+}

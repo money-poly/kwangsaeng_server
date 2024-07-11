6a35290ae23219bba94351335b181fac60a27495
KKardd <wlstjd4013@gmail.com>
2024-07-11 17:31:05 +0900

    feat: add execute-query-builder method

--- src/menus2.0/menus2.repository.ts
+++ src/menus2.0/menus2.repository.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EntityManager, Repository, SelectQueryBuilder } from 'typeorm';
+import { MenuView } from 'src/menus/entity/menu-view.entity';
+import { Menu } from 'src/menus/entity/menu.entity';
+import { ExecuteQueryBuilderType } from 'src/global/common/execute-qb-type.enum';
+import { SortFilterType } from './enum/discounte-filter-type.enum';
 
 @Injectable()
-export class Menus2Repository {}
+export class Menus2Repository {
+    constructor(
+        @InjectRepository(Menu)
+        private readonly menus: Repository<Menu>,
+        @InjectRepository(MenuView)
+        private readonly menuView: Repository<MenuView>,
+        private readonly entityManager: EntityManager,
+    ) {}
+
+    async executeQueryBuilder<T>(qb: SelectQueryBuilder<T>, type: ExecuteQueryBuilderType) {
+        switch (type) {
+            case ExecuteQueryBuilderType.MANY:
+                return qb.getRawMany();
+            case ExecuteQueryBuilderType.ONE:
+                return qb.getRawOne();
+        }
+    }
+}

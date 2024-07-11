a41890fa73312f92a7ce0b04d5368308a85bac78
KKardd <wlstjd4013@gmail.com>
2024-07-11 20:55:44 +0900

    feat: init constructor in stores module

--- src/stores2.0/stores2.controller.ts
+++ src/stores2.0/stores2.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { Stores2Service } from './stores2.service';
 
 @Controller('stores2')
-export class Stores2Controller {}
+export class Stores2Controller {
+    constructor(private readonly StoresService: Stores2Service) {}
+}

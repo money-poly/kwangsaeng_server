8a006a1479d9c538cf4fecead06d13f576a2d5d0
KKardd <wlstjd4013@gmail.com>
2024-07-12 00:45:42 +0900

    feat: add today-used-food-expenses controller

--- src/menus2.0/menus2.controller.ts
+++ src/menus2.0/menus2.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { Menus2Service } from './menus2.service';
+import { TodayUsedFoodExpensesDto } from './dto/today-used-food-expenses.dto';
 
 @Controller('menus2')
-export class Menus2Controller {}
+export class Menus2Controller {
+    constructor(private readonly menusService: Menus2Service) {}
+
+    @Get('amount-used')
+    async todayUsedFoodExpenses(@Query() dto: TodayUsedFoodExpensesDto) {
+        return this.menusService.todayUsedFoodExpenses(dto);
+    }
+}

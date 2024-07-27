4f7300bde6f0c466eed022613b81251959e2ab3d
KKardd <wlstjd4013@gmail.com>
2024-07-28 01:06:56 +0900

    feat: declare menu-categories enum

---
+++ src/menus2.0/enum/categories.enum.ts
@@ -0,0 +1,10 @@
+export enum MenuCategories {
+    KOREAN = 'korean',
+    CHINESE = 'chinese',
+    JAPANESE = 'japanese',
+    WESTERN = 'western',
+    SANCK = 'snack',
+    CAFE = 'cafe',
+    DESSERT = 'dessert',
+    FRANCHISE = 'franchise',
+}

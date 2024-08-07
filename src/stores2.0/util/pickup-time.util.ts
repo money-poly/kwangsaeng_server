f13568bce409d541566d2666c9cb107e533108f4
KKardd <wlstjd4013@gmail.com>
2024-08-07 22:00:02 +0900

    refactor: separate methods in location util

--- src/stores2.0/util/pickup-time.util.ts
+++ src/stores2.0/util/pickup-time.util.ts
@@ -1,13 +1,13 @@
-export class PickUpTimeUtil {
-    static measure(cookingTime: number, x1: number, x2: number, y1: number, y2: number): string {
-        const R = 6371.0; // 지구의 반지름 (단위: km)
+export class LocationUtil {
+    static R = 6371.0;
 
-        const toRadians = (degrees: number): number => degrees * (Math.PI / 180);
+    static toRadians = (degrees: number): number => degrees * (Math.PI / 180);
 
-        const x1Rad = toRadians(x1);
-        const y1Rad = toRadians(y1);
-        const x2Rad = toRadians(x2);
-        const y2Rad = toRadians(y2);
+    static measureDistance(lat1: number, lat2: number, lon1: number, lon2: number) {
+        const x1Rad = this.toRadians(lat1);
+        const y1Rad = this.toRadians(lon1);
+        const x2Rad = this.toRadians(lat2);
+        const y2Rad = this.toRadians(lon2);
 
         const dx = x2Rad - x1Rad;
         const dy = y2Rad - y1Rad;
@@ -15,14 +15,20 @@ export class PickUpTimeUtil {
         const a = Math.sin(dx / 2) ** 2 + Math.cos(x1Rad) * Math.cos(x2Rad) * Math.sin(dy / 2) ** 2;
         const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-        const distance = R * c; // 거리 (단위: km)
+        const distance = this.R * c * 1000; // 거리 (단위: m)
+
+        return distance;
+    }
+
+    static measurePickUpTime(cookingTime: number, lat1: number, lat2: number, lon1: number, lon2: number): string {
+        const distance = this.measureDistance(lat1, lat2, lon1, lon2);
 
         const pickUpTime: number[] = [];
-        if (distance < 0.2) {
+        if (distance < 200) {
             pickUpTime.push(5, 7);
-        } else if (distance < 0.5) {
+        } else if (distance < 500) {
             pickUpTime.push(7, 10);
-        } else if (distance < 1) {
+        } else if (distance < 1000) {
             pickUpTime.push(10, 15);
         } else {
             pickUpTime.push(15, 20);

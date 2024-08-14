45bfa7b176c5387030828e90a28b19e5ceeaa440
KKardd <wlstjd4013@gmail.com>
2024-08-15 06:44:32 +0900

    refactor: remove left-join method in store repository

--- src/stores2.0/stores2.repository.ts
+++ src/stores2.0/stores2.repository.ts
@@ -1,23 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CategoriesService } from 'src/categories/categories.service';
+import { AbstractRepository } from 'src/global/common/abstract.repository';
 import { BusinessDetail } from 'src/stores/entity/business-detail.entity';
 import { StoreApprove } from 'src/stores/entity/store-approve.entity';
 import { StoreDetail } from 'src/stores/entity/store-detail.entity';
 import { Store } from 'src/stores/entity/store.entity';
 import { StoreApproveStatus } from 'src/stores/enum/store-approve-status.enum';
 import { StoreStatus } from 'src/stores/enum/store-status.enum';
-import {
-    EntityManager,
-    FindOptionsRelations,
-    FindOptionsSelect,
-    FindOptionsWhere,
-    Repository,
-    SelectQueryBuilder,
-} from 'typeorm';
+import { EntityManager, Repository } from 'typeorm';
 
 @Injectable()
-export class Stores2Repository {
+export class Stores2Repository extends AbstractRepository<Store> {
+    protected readonly logger = new Logger(Stores2Repository.name);
+
     constructor(
         @InjectRepository(Store)
         private readonly stores: Repository<Store>,
@@ -28,31 +24,9 @@ export class Stores2Repository {
         @InjectRepository(BusinessDetail)
         private readonly businessDetails: Repository<BusinessDetail>,
         private readonly categoryService: CategoriesService,
-        public entityManager: EntityManager,
-    ) {}
-
-    async findOne(
-        where: FindOptionsWhere<Store>,
-        select?: FindOptionsSelect<Store>,
-        relations?: FindOptionsRelations<Store>,
-    ): Promise<Store> {
-        return await this.stores.findOne({
-            where,
-            select,
-            relations,
-        });
-    }
-
-    async findMany(
-        where: FindOptionsWhere<Store>,
-        select?: FindOptionsSelect<Store>,
-        relations?: FindOptionsRelations<Store>,
-    ): Promise<Store[]> {
-        return await this.stores.find({
-            where,
-            select,
-            relations,
-        });
+        entityManager: EntityManager,
+    ) {
+        super(stores, entityManager);
     }
 
     async checkApprove(store: Store): Promise<boolean> {
@@ -73,13 +47,5 @@ export class Stores2Repository {
                 status: StoreStatus.OPEN,
             },
         });
-    }
-
-    async leftJoinStoreToMenu<Menu>(qb: SelectQueryBuilder<Menu>) {
-        return qb.leftJoinAndSelect(Store, 's', 'm.store_id = s.id');
-    }
-
-    async leftJoinStoreDetail<Store>(qb: SelectQueryBuilder<Store>) {
-        return qb.leftJoinAndSelect(StoreDetail, 'sd', 's.id = sd.store_id');
     }
 }

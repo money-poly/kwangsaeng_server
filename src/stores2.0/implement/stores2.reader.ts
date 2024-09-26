ff19c020d985585b43f4f9ebbaf08302ca21062f
KKardd <wlstjd4013@gmail.com>
2024-09-27 01:24:04 +0900

    feat: add read-one-by-user implement

--- src/stores2.0/implement/stores2.reader.ts
+++ src/stores2.0/implement/stores2.reader.ts
@@ -5,7 +5,19 @@ import { Stores2Repository } from '../stores2.repository';
 export class Stores2Reader {
     constructor(private readonly storesRepository: Stores2Repository) {}
 
-    async read(id: number) {
-        return await this.storesRepository.findOne({ id }, {}, { approve: true, detail: true, businessDetail: true });
+    async readOne(id: number) {
+        return await this.storesRepository.findOne(
+            { id },
+            {},
+            { approve: true, detail: true, businessDetail: true, user: true },
+        );
+    }
+
+    async readOneByUser(userId: number) {
+        return await this.storesRepository.findOne(
+            { user: { id: userId } },
+            {},
+            { approve: true, detail: true, businessDetail: true, user: true },
+        );
     }
 }

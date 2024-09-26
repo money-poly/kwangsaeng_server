13ece7fa2e09cda8ace277a865589a952d66ec3e
KKardd <wlstjd4013@gmail.com>
2024-09-27 01:35:46 +0900

    feat: create add-menu-order service

--- src/stores2.0/implement/stores2.appender.ts
+++ src/stores2.0/implement/stores2.appender.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { Stores2Repository } from '../stores2.repository';
+import { Store } from 'src/stores/entity/store.entity';
 
 @Injectable()
 export class Stores2Appender {
     constructor(private readonly storesRepository: Stores2Repository) {}
+
+    async addMenuOrder(store: Store, menuId: number) {
+        const order = store.detail.menuOrders ? store.detail.menuOrders : [];
+        order.unshift(menuId);
+        const newStore = new Store({ ...store });
+        newStore.detail.menuOrders = order;
+
+        return await this.storesRepository.findOneAndUpdate({ id: store.id }, newStore);
+    }
 }
